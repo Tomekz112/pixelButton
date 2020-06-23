d723f586f90fb3c0118d530bb57a18bd0403e5a4
Tomekz112 <61382360+Tomekz112@users.noreply.github.com>
2020-06-24 01:11:11 +0200

    Update buttonType.go

--- buttonType.go
+++ buttonType.go
@@ -22,17 +22,20 @@ type buttonType struct {
 	textSize            float64
 }
 
+//CreateButton at give position, and given text
 //position 0,0 is down left corner
-func (b *buttonType) createButton(buttonPos pixel.Vec, text string) {
+func CreateButton(buttonPos pixel.Vec, text string) {
 	buttonsPos = append(buttonsPos, buttonPos)
 	buttonsText = append(buttonsText, text)
 }
 
-func (b *buttonType) returnButtons() ([]string, []pixel.Vec) {
+//ReturnButtons returns slice in which there are all buttons text and position
+func ReturnButtons() ([]string, []pixel.Vec) {
 	return buttonsText, buttonsPos
 }
 
-func (b *buttonType) drawButtons(win pixel.Target, min, max int, textSize float64) {
+//DrawButtons draw buttons from given position(min) to given position(max) in slice
+func DrawButtons(win pixel.Target, min, max int, textSize float64) {
 	for i := min; i-1 < max; i++ {
 		buttonText = text.New(pixel.V(buttonsPos[i].X, buttonsPos[i].Y), basicAtlas)
 		fmt.Fprintln(buttonText, buttonsText[i])
@@ -42,7 +45,9 @@ func (b *buttonType) drawButtons(win pixel.Target, min, max int, textSize float6
 	return
 }
 
-func (b *buttonType) interactButton(mousePos pixel.Vec, textSize float64) (bool, int) {
+//InteractButton return true whenever mouse cursor is on button and number of button moues cursor
+//is interacting with
+func InteractButton(mousePos pixel.Vec, textSize float64) (bool, int) {
 	i := 0
 	for range buttonsText {
 		buttonText = text.New(pixel.V(buttonsPos[i].X, buttonsPos[i].Y), basicAtlas)
@@ -57,4 +62,13 @@ func (b *buttonType) interactButton(mousePos pixel.Vec, textSize float64) (bool,
 		i++
 	}
 	return false, 0
+}
+
+//DeleteButtons deletes button from min to max
+//ex. we have slice with [0 1 2 3 4 5 6 7 8 9],
+//if min = 9 and max = 10
+//the slice will be:[1 2 3 4 5 6 7 8 10]
+func DeleteButtons(min, max int) {
+	buttonsPos = append(buttonsPos[:min], buttonsPos[max:]...)
+	buttonsText = append(buttonsText[:min], buttonsText[max:]...)
 }
